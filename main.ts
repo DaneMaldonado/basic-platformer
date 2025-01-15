3b9d94ea94a68f442a943f6601fc66bc450c9daa
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-01-15 18:35:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,105 @@
+function SpawnEnemy () {
+    for (let value of tiles.getTilesByType(assets.tile`myTile6`)) {
+        Guard = sprites.create(img`
+            ...............ff.......
+            .............ff2ffff....
+            ............ff2feeeeff..
+            ...........ff22feeeeeff.
+            ...........feeeeffeeeef.
+            ..........fe2222eefffff.
+            ..........f2effff222efff
+            ..........fffeeeffffffff
+            ..........fee44fbe44efef
+            ...........feddfb4d4eef.
+            ..........c.eeddd4eeef..
+            ....ccccccceddee2222f...
+            .....dddddcedd44e444f...
+            ......ccccc.eeeefffff...
+            ..........c...ffffffff..
+            ...............ff..fff..
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy)
+        tiles.placeOnTile(Guard, value)
+        tiles.setTileAt(value, assets.tile`transparency16`)
+        Coins.scale = 1.4
+    }
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    game.gameOver(false)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Hero.setVelocity(0, -130)
+})
+function SpawnPlayer () {
+    for (let value of tiles.getTilesByType(assets.tile`myTile1`)) {
+        Hero = sprites.create(img`
+            . . . . . f f f f f f . . . . . 
+            . . . f f e e e e f 2 f . . . . 
+            . . f f e e e e f 2 2 2 f . . . 
+            . . f e e e f f e e e e f . . . 
+            . . f f f f e e 2 2 2 2 e f . . 
+            . . f e 2 2 2 f f f f e 2 f . . 
+            . f f f f f f f e e e f f f . . 
+            . f f e 4 4 e b f 4 4 e e f . . 
+            . f e e 4 d 4 1 f d d e f f . . 
+            . . f e e e 4 d d d d f d d f . 
+            . . . f f e e 4 e e e f b b f . 
+            . . . . f 2 2 2 4 d d e b b f . 
+            . . . . e 2 2 2 e d d e b f . . 
+            . . . . f 4 4 4 f e e f f . . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . . f f f . . . . . . . 
+            `, SpriteKind.Player)
+        Hero.scale = 1.15
+        tiles.placeOnTile(Hero, value)
+        tiles.setTileAt(value, assets.tile`transparency16`)
+        scene.cameraFollowSprite(Hero)
+        controller.moveSprite(Hero, 100, 0)
+        Hero.ay = 160
+    }
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    info.changeScoreBy(1)
+    sprites.destroy(Coins)
+})
+function Coin () {
+    for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
+        Coins = sprites.create(img`
+            . . b b b b . . 
+            . b 5 5 5 5 b . 
+            b 5 d 3 3 d 5 b 
+            b 5 3 5 5 1 5 b 
+            c 5 3 5 5 1 d c 
+            c d d 1 1 d d c 
+            . f d d d d f . 
+            . . f f f f . . 
+            `, SpriteKind.Food)
+        tiles.placeOnTile(Coins, value)
+        tiles.setTileAt(value, assets.tile`transparency16`)
+        Coins.scale = 1.4
+    }
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    game.gameOver(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.gameOver(false)
+})
+let Hero: Sprite = null
+let Coins: Sprite = null
+let Guard: Sprite = null
+tiles.setCurrentTilemap(tilemap`level2`)
+info.setScore(0)
+SpawnPlayer()
+Coin()
+SpawnEnemy()
+forever(function () {
+    Guard.setVelocity(-5, 0)
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,86 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`32000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000050005000000000000000000000000050005000000000000000000000000000000000000000000000000000000000000000202020200000000000000000000000202020200000000000000000000000000000000000000000000000000000000000000000000000000000000050005000000000000000000000002000000000000000000000000000000000000000000050005000000000000000000000202020200000000000000050500000002000500050000000000000000000000000000000202020200000000050500000000000000000000000000000000000000000002020202000000000000000000030000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000600010100000000000000000000000000000000020202020200000202020202020200000000020202020202020202020202020001010002020202020202020202020202020200000000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000000000101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+..................................................
+..................................................
+..................................................
+.............2222...........2222..................
+....................................2.............
+.....................2222............2............
+.......2222...........................2222........
+..................................................
+..................................................
+22222..2222222....2222222222222....222222222222222
+..................................................
+..................................................
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile8], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile6":
+            case "tile8":return tile8;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
