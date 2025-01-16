448aaefd56c3b0fb0a3efd683a1df14c9a78f066
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-01-16 11:48:52 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -28,6 +28,112 @@ function SpawnEnemy () {
             `, SpriteKind.Enemy)
         tiles.placeOnTile(Guard, value)
         tiles.setTileAt(value, assets.tile`transparency16`)
+        animation.runImageAnimation(
+        Guard,
+        [img`
+            ..............ffffff....
+            .............f2feeeeff..
+            ............f222feeeeff.
+            .......cc...feeeeffeeef.
+            .......cdc.fe2222eeffff.
+            .......cddcf2effff222ef.
+            ........cddcffeeefffffff
+            .........cddce44fbe44eff
+            ..........cdceddf14d4eef
+            ..........cccdeddd4eeef.
+            ...........edd4e44eeff..
+            ............ee442222f...
+            .............f2e2222f...
+            .............f554444f...
+            ..............ffffff....
+            ................fff.....
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            `,img`
+            ........................
+            ..............fff.......
+            .............f2fffff....
+            ...........ff22eeeeeff..
+            ..........ff222eeeeeeff.
+            ..........feeeefffeeeef.
+            .........fe2222eeefffff.
+            .........f2efffff222efff
+            ..cc.....fffeeefffffffff
+            ..cdcc...fee44fbbe44efef
+            ..ccddcc..feddfbb4d4eef.
+            ....cdddceefddddd4eeef..
+            .....ccdcddee2222222f...
+            ......cccdd44e544444f...
+            .........eeeeffffffff...
+            .............ff...fff...
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            `,img`
+            ...............ff.......
+            .............ff2ffff....
+            ............ff2feeeeff..
+            ...........ff22feeeeeff.
+            ...........feeeeffeeeef.
+            ..........fe2222eefffff.
+            ..........f2effff222efff
+            ..........fffeeeffffffff
+            ..........fee44fbe44efef
+            ...........feddfb4d4eef.
+            ..........c.eeddd4eeef..
+            ....ccccccceddee2222f...
+            .....dddddcedd44e444f...
+            ......ccccc.eeeefffff...
+            ..........c...ffffffff..
+            ...............ff..fff..
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            `,img`
+            ..............ffffff....
+            .............f2feeeeff..
+            ............f222feeeeff.
+            ............feeeeffeeef.
+            ...........fe2222eeffff.
+            ...........f2effff222ef.
+            ...........fffeeefffffff
+            ...........fee44fbe44eff
+            ............feddf14d4eef
+            .............fdddd4eeef.
+            .............fe444eddf..
+            .............ccc22eddf..
+            .............cdc22fee...
+            ............cddc4444f...
+            ...........cddcfffff....
+            ..........cddc..fff.....
+            ..........cdc...........
+            ..........cc............
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            `],
+        1000,
+        true
+        )
         Coins.scale = 1.4
     }
 }
@@ -35,27 +141,29 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, lo
     game.gameOver(false)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    Hero.setVelocity(0, -130)
+    if (Hero.vy == 0) {
+        Hero.setVelocity(0, -130)
+    }
 })
 function SpawnPlayer () {
     for (let value of tiles.getTilesByType(assets.tile`myTile1`)) {
         Hero = sprites.create(img`
-            . . . . . f f f f f f . . . . . 
-            . . . f f e e e e f 2 f . . . . 
-            . . f f e e e e f 2 2 2 f . . . 
-            . . f e e e f f e e e e f . . . 
-            . . f f f f e e 2 2 2 2 e f . . 
-            . . f e 2 2 2 f f f f e 2 f . . 
-            . f f f f f f f e e e f f f . . 
-            . f f e 4 4 e b f 4 4 e e f . . 
-            . f e e 4 d 4 1 f d d e f f . . 
-            . . f e e e 4 d d d d f d d f . 
-            . . . f f e e 4 e e e f b b f . 
-            . . . . f 2 2 2 4 d d e b b f . 
-            . . . . e 2 2 2 e d d e b f . . 
-            . . . . f 4 4 4 f e e f f . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . . f f f . . . . . . . 
+            . . . . . . 5 . 5 . . . . . . . 
+            . . . . . f 5 5 5 f f . . . . . 
+            . . . . f 1 5 2 5 1 6 f . . . . 
+            . . . f 1 6 6 6 6 6 1 6 f . . . 
+            . . . f 6 6 f f f f 6 1 f . . . 
+            . . . f 6 f f d d f f 6 f . . . 
+            . . f 6 f d f d d f d f 6 f . . 
+            . . f 6 f d 3 d d 3 d f 6 f . . 
+            . . f 6 6 f d d d d f 6 6 f . . 
+            . f 6 6 f 3 f f f f 3 f 6 6 f . 
+            . . f f d 3 5 3 3 5 3 d f f . . 
+            . . f d d f 3 5 5 3 f d d f . . 
+            . . . f f 3 3 3 3 3 3 f f . . . 
+            . . . f 3 3 5 3 3 5 3 3 f . . . 
+            . . . f f f f f f f f f f . . . 
+            . . . . . f f . . f f . . . . . 
             `, SpriteKind.Player)
         Hero.scale = 1.15
         tiles.placeOnTile(Hero, value)
@@ -65,9 +173,123 @@ function SpawnPlayer () {
         Hero.ay = 160
     }
 }
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
-    info.changeScoreBy(1)
-    sprites.destroy(Coins)
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Hero,
+    [img`
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 6 2 5 5 6 f . . . . . 
+        . . . f 6 6 6 6 1 6 6 f . . . . 
+        . . . f 6 6 6 6 6 1 6 f . . . . 
+        . . . f d f d 6 6 6 1 f . . . . 
+        . . . f d f d 6 6 6 6 f f . . . 
+        . . . f d 3 d d 6 6 6 f 6 f . . 
+        . . . . f d d d f f 6 f f . . . 
+        . . . . . f f 5 3 f 6 6 6 f . . 
+        . . . . f 5 3 3 f f f f f . . . 
+        . . . . f 3 3 f d f . . . . . . 
+        . . . . . f 3 f d f . . . . . . 
+        . . . . f 3 5 3 f d f . . . . . 
+        . . . . f f 3 3 f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 . 5 . . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . f 6 2 5 5 6 f . . . . 
+        . . . . f 6 6 6 6 1 6 6 f . . . 
+        . . . . f 6 6 6 6 6 1 6 f . . . 
+        . . . . f d f d 6 6 6 1 f . . . 
+        . . . . f d f d 6 6 6 6 f f . . 
+        . . . . f d 3 d d 6 6 6 f 6 f . 
+        . . . . . f d d d f f 6 f f . . 
+        . . . . . . f f 3 3 f f 6 6 f . 
+        . . . . . f d d d d f f f f . . 
+        . . . . . f d d d f 3 f . . . . 
+        . . . . . . f f f d 5 3 f . . . 
+        . . . . . f f f 3 3 f f . . . . 
+        . . . . . f f f f f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 . 5 . . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . f 6 2 5 5 6 f . . . . 
+        . . . . f 6 6 6 6 1 6 6 f . . . 
+        . . . . f 6 6 6 6 6 1 6 f . . . 
+        . . . . f d f d 6 6 6 1 f . . . 
+        . . . . f d f d 6 6 6 6 f f . . 
+        . . . . f d 3 d d 6 6 6 f 6 f . 
+        . . . . . f d d d f f 6 f f . . 
+        . . . . . . f f 3 3 f f 6 6 f . 
+        . . . . . f 5 3 3 d d f f f . . 
+        . . . . . f 3 3 3 f d d f . . . 
+        . . . . . . f 3 5 f f f . . . . 
+        . . . . . f 3 3 3 3 f . . . . . 
+        . . . . . . f f f f f . . . . . 
+        `],
+    100,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Hero,
+    [img`
+        . . . . . . . 5 . 5 . . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . f 6 5 5 2 6 f . . . . 
+        . . . . f 6 6 1 6 6 6 6 f . . . 
+        . . . . f 6 1 6 6 6 6 6 f . . . 
+        . . . . f 1 6 6 6 d f d f . . . 
+        . . . f f 6 6 6 6 d f d f . . . 
+        . . f 6 f 6 6 6 d d 3 d f . . . 
+        . . . f f 6 f f d d d f . . . . 
+        . . f 6 6 6 f 3 5 f f . . . . . 
+        . . . f f f f f 3 3 5 f . . . . 
+        . . . . . . f d f 3 3 f . . . . 
+        . . . . . . f d f 3 f . . . . . 
+        . . . . . f d f 3 5 3 f . . . . 
+        . . . . . . f f 3 3 f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 6 5 5 2 6 f . . . . . 
+        . . . f 6 6 1 6 6 6 6 f . . . . 
+        . . . f 6 1 6 6 6 6 6 f . . . . 
+        . . . f 1 6 6 6 d f d f . . . . 
+        . . f f 6 6 6 6 d f d f . . . . 
+        . f 6 f 6 6 6 d d 3 d f . . . . 
+        . . f f 6 f f d d d f . . . . . 
+        . f 6 6 f f 3 3 f f . . . . . . 
+        . . f f f f d d d d f . . . . . 
+        . . . . f 3 f d d d f . . . . . 
+        . . . f 3 5 d f f f . . . . . . 
+        . . . . f f 3 3 f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 6 5 5 2 6 f . . . . . 
+        . . . f 6 6 1 6 6 6 6 f . . . . 
+        . . . f 6 1 6 6 6 6 6 f . . . . 
+        . . . f 1 6 6 6 d f d f . . . . 
+        . . f f 6 6 6 6 d f d f . . . . 
+        . f 6 f 6 6 6 d d 3 d f . . . . 
+        . . f f 6 f f d d d f . . . . . 
+        . f 6 6 f f 3 3 f f . . . . . . 
+        . . f f f d d 3 3 5 f . . . . . 
+        . . . f d d f 3 3 3 f . . . . . 
+        . . . . f f f 5 3 f . . . . . . 
+        . . . . . f 3 3 3 3 f . . . . . 
+        . . . . . f f f f f . . . . . . 
+        `],
+    100,
+    true
+    )
 })
 function Coin () {
     for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
@@ -84,10 +306,80 @@ function Coin () {
         tiles.placeOnTile(Coins, value)
         tiles.setTileAt(value, assets.tile`transparency16`)
         Coins.scale = 1.4
+        animation.runImageAnimation(
+        Coins,
+        [img`
+            . . b b b b . . 
+            . b 5 5 5 5 b . 
+            b 5 d 3 3 d 5 b 
+            b 5 3 5 5 1 5 b 
+            c 5 3 5 5 1 d c 
+            c d d 1 1 d d c 
+            . f d d d d f . 
+            . . f f f f . . 
+            `,img`
+            . . b b b . . . 
+            . b 5 5 5 b . . 
+            b 5 d 3 d 5 b . 
+            b 5 3 5 1 5 b . 
+            c 5 3 5 1 d c . 
+            c 5 d 1 d d c . 
+            . f d d d f . . 
+            . . f f f . . . 
+            `,img`
+            . . . b b . . . 
+            . . b 5 5 b . . 
+            . b 5 d 1 5 b . 
+            . b 5 3 1 5 b . 
+            . c 5 3 1 d c . 
+            . c 5 1 d d c . 
+            . . f d d f . . 
+            . . . f f . . . 
+            `,img`
+            . . . b b . . . 
+            . . b 5 5 b . . 
+            . . b 1 1 b . . 
+            . . b 5 5 b . . 
+            . . b d d b . . 
+            . . c d d c . . 
+            . . c 3 3 c . . 
+            . . . f f . . . 
+            `,img`
+            . . . b b . . . 
+            . . b 5 5 b . . 
+            . b 5 1 d 5 b . 
+            . b 5 1 3 5 b . 
+            . c d 1 3 5 c . 
+            . c d d 1 5 c . 
+            . . f d d f . . 
+            . . . f f . . . 
+            `,img`
+            . . . b b b . . 
+            . . b 5 5 5 b . 
+            . b 5 d 3 d 5 b 
+            . b 5 1 5 3 5 b 
+            . c d 1 5 3 5 c 
+            . c d d 1 d 5 c 
+            . . f d d d f . 
+            . . . f f f . . 
+            `],
+        500,
+        true
+        )
     }
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
-    game.gameOver(true)
+    if (info.score() == 14) {
+        game.gameOver(true)
+    } else {
+        game.splash("You need more coins!")
+        tiles.placeOnTile(sprite, tiles.getTileLocation(1, 5))
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite)
+    music.play(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     game.gameOver(false)
